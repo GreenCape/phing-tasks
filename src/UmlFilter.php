9658b9ec2c5115e704342ad43019f778983bd9c0
Niels Braczek <nbraczek@bsds.de>
2019-08-14 14:28:51 +0200

    Minor refactoring

--- src/UmlFilter.php
+++ src/UmlFilter.php
@@ -2,8 +2,14 @@
 
 namespace GreenCape\PhingTasks;
 
-class UmlFilter extends \Task
+use BuildException;
+use Task;
+
+class UmlFilter extends Task
 {
+	const OP_EXTENDS = '<|--';
+	const OP_IMPLEMENTS = '<|..';
+
 	protected $file;
 	protected $dir;
 	protected $skin;
@@ -35,6 +41,9 @@ class UmlFilter extends \Task
 		$this->jar = $jar;
 	}
 
+	/**
+	 * @throws BuildException
+	 */
 	public function main()
 	{
 		$this->validate();
@@ -52,22 +61,25 @@ class UmlFilter extends \Task
 		$this->render();
 	}
 
+	/**
+	 * @throws BuildException
+	 */
 	protected function validate()
 	{
 		if (empty($this->jar))
 		{
-			throw new \BuildException("Please provide location of plantuml.jar");
+			throw new BuildException('Please provide location of plantuml.jar');
 		}
 
-		if (count($this->fileSets) == 0 && count($this->fileLists) == 0)
+		if (count($this->fileSets) === 0 && count($this->fileLists) === 0)
 		{
-			throw new \BuildException("Need either nested fileset or nested filelist to iterate through");
+			throw new BuildException('Need either nested fileset or nested filelist to iterate through');
 		}
 	}
 
 	/**
 	 * @return array
-	 * @throws \BuildException
+	 * @throws BuildException
 	 */
 	protected function handleFiles()
 	{
@@ -79,7 +91,7 @@ class UmlFilter extends \Task
 
 			foreach ($this->generateDiagramSource($code) as $group => $fragments)
 			{
-				$aggregate[$group] = array_merge((array)$aggregate[$group], $fragments);
+				$aggregate[$group] = array_merge((array) $aggregate[$group], $fragments);
 			}
 		}
 
@@ -87,111 +99,83 @@ class UmlFilter extends \Task
 	}
 
 	/**
-	 * @param $matches
+	 * @param $code
+	 *
+	 * @return array
 	 */
 	protected function generateDiagramSource($code)
 	{
-		$identifier = '([\S]+)';
-
-		$namespace = '';
-
-		if (preg_match('~namespace\s+(.*?);~', $code, $match))
-		{
-			$namespace = trim(str_replace('\\', '.', $match[1]), '.') . '.';
-		}
-
-		$declaration = '(abstract\s+class|interface|trait|class)\s+' . $identifier;
-		$extends     = '\s+extends\s+' . $identifier;
-		$implements  = '\s+implements\s+' . $identifier . '(:?\s*,\s*' . $identifier . ')*';
-		$pattern     = "~{$declaration}(:?{$extends})?(:?{$implements})?\s*\{~";
-
-		if (!preg_match_all($pattern, $code, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER))
-		{
-			return array();
-		}
-
-		$classes = array();
-
-		for ($i = 0, $n = count($matches); $i < $n; $i++)
-		{
-			if (isset($matches[$i + 1]))
-			{
-				$classes[$i] = substr($code, $matches[$i][0][1], $matches[$i + 1][0][1] - $matches[$i][0][1]);
-			}
-			else
-			{
-				$classes[$i] = substr($code, $matches[$i][0][1]);
-			}
-		}
-
+		$namespace = $this->findNamespace($code);
+		$uses      = $this->findUseStatements($code);
+		$classes   = $this->findClassDeclaration($code);
 		$aggregate = $this->prepareGroups($namespace);
 
-		foreach ($matches as $i => $match)
+		foreach ($classes as $info)
 		{
-			$uml          = '';
-			$currentClass = $namespace . $match[2][0];
+			$currentClass = $this->fullyQualifiedName($namespace, $info['classname'], $uses);
 			$filename     = $this->dir . '/class-' . $currentClass . '.puml';
-			$uml .= "{$match[1][0]} {$currentClass}\n";
+			$uml          = "{$info['declaration']} {$currentClass}\n";
 
-			if (!empty($match[4][0]))
+			if (!empty($info['extends']))
 			{
-				$uml .= $this->handleReference($namespace, $currentClass, '<|--', $match[4][0]);
+				$uml .= $this->handleReference($currentClass, self::OP_EXTENDS, $this->fullyQualifiedName($namespace, $info['extends'], $uses));
 			}
 
-			if (!empty($match[6][0]))
+			foreach ($info['implements'] as $interface)
 			{
-				$uml .= $this->handleReference($namespace, $currentClass, '<|..', $match[6][0]);
+				$uml .= $this->handleReference($currentClass, self::OP_IMPLEMENTS, $this->fullyQualifiedName($namespace, $interface, $uses));
 			}
 
-			$this->writePuml($filename, $uml);
+			$this->writePuml($filename, trim($uml));
 			$this->log("Generated class diagram for {$currentClass}");
 
 			foreach ($aggregate as $level => $levelCode)
 			{
+				if ($aggregate[$level] === '')
+				{
+					$aggregate[$level] = [];
+				}
+
 				$aggregate[$level][] = $uml;
 			}
 
-			$this->handleMethods($currentClass, $classes[$i]);
+			$this->handleMethods($currentClass, $info['code']);
 		}
 
 		return $aggregate;
 	}
 
 	/**
-	 * @param $namespace
 	 * @param $class
 	 * @param $op
 	 * @param $reference
 	 *
 	 * @return string
 	 */
-	protected function handleReference($namespace, $class, $op, $reference)
+	protected function handleReference($class, $op, $reference)
 	{
-		$reference = str_replace('\\', '.', $reference);
-		$reference = $reference[0] == '.' ? substr($reference, 1) : $namespace . $reference;
-		$uml       = "{$reference} {$op} {$class}\n";
-		$uml .= $this->includeReferencedClass($reference);
-
-		return $uml;
+		return $this->includeReferencedClass($reference, $op) . "{$reference} {$op} {$class}\n";
 	}
 
 	/**
 	 * @param $class
 	 *
-	 * @return array
+	 * @param $op
+	 *
+	 * @return string
 	 */
-	protected function includeReferencedClass($class)
+	protected function includeReferencedClass($class, $op)
 	{
-		$uml = '';
-
 		if ($this->includeRef)
 		{
-			$file = "{$this->dir}/class-{$class}.puml";
-			$uml  = "!include {$file}\n";
-			touch($file);
+			$filename = "{$this->dir}/class-{$class}.puml";
+			$uml      = ($op === self::OP_IMPLEMENTS ? 'interface ' : 'class ') . $class;
+			file_put_contents($filename, "@startuml\n!include skin.puml\n{$uml}\n@enduml\n");
+
+			return "!include {$filename}\n";
 		}
 
-		return $uml;
+		return '';
 	}
 
 	/**
@@ -200,7 +184,7 @@ class UmlFilter extends \Task
 	 */
 	protected function handleMethods($class, $code)
 	{
-		$pattern = "~@startuml\n(.*?)@enduml.*?(private|protected|public)?\s+function\s+(\S+)\s*\(~sm";
+		$pattern = "~@startuml\n(.*?)@enduml.*?(private|protected|public|static|final)?\s+function\s+(\S+)\s*\(~sm";
 
 		if (!preg_match_all($pattern, $code, $matches, PREG_SET_ORDER))
 		{
@@ -213,8 +197,6 @@ class UmlFilter extends \Task
 			$this->writePuml($this->dir . '/seq-' . $methodName . '.puml', implode("\n", preg_split("~\s+\*\s+~", $match[1])) . "\n");
 			$this->log("Extracted diagram for {$methodName}()");
 		}
-
-		return;
 	}
 
 	/**
@@ -223,7 +205,12 @@ class UmlFilter extends \Task
 	 */
 	protected function writePuml($filename, $uml)
 	{
-		file_put_contents($filename, "@startuml\n!include skin.puml\n{$uml}@enduml\n");
+		if (empty($uml))
+		{
+			return;
+		}
+
+		file_put_contents($filename, "@startuml\n!include skin.puml\n{$uml}\n@enduml\n");
 	}
 
 	/**
@@ -233,8 +220,7 @@ class UmlFilter extends \Task
 	 */
 	protected function removeIncludes($uml)
 	{
-		$uml = array_filter(explode("\n", implode("\n", $uml)), function ($line)
-		{
+		$uml = array_filter(explode("\n", implode("\n", $uml)), static function ($line) {
 			return !preg_match('~^!include~', $line);
 		});
 
@@ -243,27 +229,141 @@ class UmlFilter extends \Task
 
 	protected function render()
 	{
-		$this->log("Rendering ...");
-		`java -jar '{$this->jar}' -tsvg '{$this->dir}/*.puml'`;
-		$this->log("... done.");
+		$this->log('Rendering ...');
+		shell_exec("java -jar '{$this->jar}' -tsvg -quiet '{$this->dir}/*.puml'");
+		$this->log('... done.');
 	}
 
 	/**
 	 * @param $namespace
 	 *
-	 * @return array
+	 * @return array[]
 	 */
 	protected function prepareGroups($namespace)
 	{
-		$aggregate = array('global' => '');
+		$aggregate = array('global' => []);
 		$currLevel = '';
 		$parts     = explode('.', $namespace);
 		while (!empty($parts))
 		{
 			$currLevel             = trim($currLevel . '.' . array_shift($parts), '.');
-			$aggregate[$currLevel] = '';
+			$aggregate[$currLevel] = [];
 		}
 
 		return $aggregate;
-}
+	}
+
+	/**
+	 * @param string $namespace
+	 * @param string $classname
+	 * @param array  $uses
+	 *
+	 * @return mixed|string
+	 */
+	protected function fullyQualifiedName($namespace, $classname, array $uses)
+	{
+		if ($classname[0] === '\\')
+		{
+			return $this->dotNotation(substr($classname, 1));
+		}
+
+		if (isset($uses[$classname]))
+		{
+			return $this->dotNotation($uses[$classname]);
+		}
+
+		return $this->dotNotation($namespace . $classname);
+	}
+
+	protected function findUseStatements($code)
+	{
+		$uses       = [];
+		$identifier = '([\S]+)';
+
+		if (preg_match_all('~\b' . 'use\s+([a-z\\\]+?)(?:\s+as\s+' . $identifier . ')?;~i', $code, $useStatements, PREG_SET_ORDER))
+		{
+			foreach ($useStatements as $use)
+			{
+				if (isset($use[2]))
+				{
+					$alias = $use[2];
+				}
+				else
+				{
+					$alias = preg_replace('~.*\\\~', '', $use[1]);
+				}
+
+				$uses[$alias] = $use[1];
+			}
+		}
+
+		return $uses;
+	}
+
+	protected function findNamespace($code)
+	{
+		$namespace = '';
+
+		if (preg_match('~\b' . 'namespace\s+([a-z\\\]+?);~i', $code, $match))
+		{
+			$namespace = $match[1] . '\\';
+		}
+
+		return $namespace;
+	}
+
+	protected function findClassDeclaration($code)
+	{
+		$result = [];
+
+		$identifier  = '([\S]+)';
+		$declaration = '(abstract\s+class|interface|trait|class)\s+' . $identifier;
+		$extends     = '\s+extends\s+' . $identifier;
+		$implements  = '\s+implements\s+' . $identifier . '(:?\s*,\s*' . $identifier . ')*';
+		$pattern     = "~{$declaration}(:?{$extends})?(:?{$implements})?\s*\{~";
+
+		if (!preg_match_all($pattern, $code, $matches, PREG_OFFSET_CAPTURE | PREG_SET_ORDER))
+		{
+			return array();
+		}
+
+		// Separate classes
+		foreach ($matches as $i => $match)
+		{
+			if (isset($matches[$i + 1]))
+			{
+				$body = substr($code, $match[0][1], $matches[$i + 1][0][1] - $match[0][1]);
+			}
+			else
+			{
+				$body = substr($code, $match[0][1]);
+			}
+
+			$implements = [];
+			for ($index = 6; isset($match[$index]); $index += 2)
+			{
+				$implements[] = $match[$index][0];
+			}
+
+			$result[$i] = [
+				'declaration' => $match[1][0],
+				'classname'   => $match[2][0],
+				'extends'     => isset($match[4]) ? $match[4][0] : null,
+				'implements'  => $implements,
+				'code'        => $body
+			];
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param $subject
+	 *
+	 * @return string
+	 */
+	protected function dotNotation($subject)
+	{
+		return trim(str_replace('\\', '.', $subject), '.');
+	}
 }
